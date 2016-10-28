6b7f53fd953db833faa6959ddff297df39cb6dbe
nc415 <njcollins@live.co.uk>
2016-10-28 22:16:27 +0100

    additional files and changes for heroku

--- Friend/settings.py
+++ Friend/settings.py
@@ -168,3 +168,17 @@ SOCIALACCOUNT_PROVIDERS = { 'google':
         { 'SCOPE': ['profile', 'email'],
           'AUTH_PARAMS': { 'access_type': 'online' } }
 }
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
