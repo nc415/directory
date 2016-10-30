567e6351816cbb33be8bfaaa48775648b4bdc257
nc415 <njcollins@live.co.uk>
2016-10-30 08:56:56 +0000

    exception catching in populate

--- populate.py
+++ populate.py
@@ -8,25 +8,28 @@ from datetime import datetime
 import csv 
 
 with open('friendlist.csv', 'r') as csvfile:
-	friendreader=csv.reader(csvfile, delimiter=',', quotechar='|')
-	for row in friendreader:
-		
-		print (row[0])
-
-		def populate():
-
-			person1 = add_person(name=row[0], rank=row[1], fb=row[2], profile=row[3] )
+	try:
+		friendreader=csv.reader(csvfile, delimiter=',', quotechar='|')
+		for row in friendreader:
 			
+			print (row[0])
 
-		def add_person(name, rank, fb, profile):
-			User_Item=User.objects.get(email="njcollins@live.co.uk")
-			c=Person.objects.get_or_create(name=name, rank=rank,profile_picture=profile, FB_link=fb, user=User_Item, classification="F")
-			return c
+			def populate():
 
-		def add_page(person, title):
-			p=Page.objects.get_or_create(title=title, person=person)[0]
-			return p
+				person1 = add_person(name=row[0], rank=row[1], fb=row[2], profile=row[3] )
+				
 
-		if __name__ == '__main__':
-			print("Starting population script")
-			populate()
+			def add_person(name, rank, fb, profile):
+				User_Item=User.objects.get(email="njcollins@live.co.uk")
+				c=Person.objects.get_or_create(name=name, rank=rank,profile_picture=profile, FB_link=fb, user=User_Item, classification="F")
+				return c
+
+			def add_page(person, title):
+				p=Page.objects.get_or_create(title=title, person=person)[0]
+				return p
+
+			if __name__ == '__main__':
+				print("Starting population script")
+				populate()
+	except UnicodeDecodeError():
+		pass
