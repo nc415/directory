048254e067544faa595745bc205839045ce58c0e
nc415 <njcollins@live.co.uk>
2016-10-30 09:02:25 +0000

    populate edit

--- populate.py
+++ populate.py
@@ -8,21 +8,25 @@ from datetime import datetime
 import csv 
 
 with open('friendlist.csv', 'r') as csvfile:
-	try:
+	
 		friendreader=csv.reader(csvfile, delimiter=',', quotechar='|')
 		for row in friendreader:
 			
 			print (row[0])
-
+		
 			def populate():
 
 				person1 = add_person(name=row[0], rank=row[1], fb=row[2], profile=row[3] )
 				
 
 			def add_person(name, rank, fb, profile):
-				User_Item=User.objects.get(email="njcollins@live.co.uk")
-				c=Person.objects.get_or_create(name=name, rank=rank,profile_picture=profile, FB_link=fb, user=User_Item, classification="F")
-				return c
+				try:
+					User_Item=User.objects.get(email="njcollins@live.co.uk")
+					c=Person.objects.get_or_create(name=name, rank=rank,profile_picture=profile, FB_link=fb, user=User_Item, classification="F")
+					return c
+				except UnicodeDecodeError:
+					print ("Unicode Decode Error, Passing....")
+					pass
 
 			def add_page(person, title):
 				p=Page.objects.get_or_create(title=title, person=person)[0]
@@ -31,6 +35,4 @@ with open('friendlist.csv', 'r') as csvfile:
 			if __name__ == '__main__':
 				print("Starting population script")
 				populate()
-	except UnicodeDecodeError:
-		print ("Unicode Decode Error, Passing....")
-		pass
+	
